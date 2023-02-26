94255e80f15169cbc54f1231c9bd35bfdc486d32
hubbleDJ <makscum@gmail.com>
2023-02-26 23:23:47 +0300

    реализован эхо бот

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import modules.HubbleTg as hubTg
+import asyncio
 from config import TOKEN
 
 bot = hubTg.HubbleTg(TOKEN)
 
-print()
+while True:
+    messages = asyncio.run(bot.get_messages())
+    if messages['ok']:
+        for message in messages['messages']:
+            print(asyncio.run(bot.send_message(message['text'], message['chat']['id'], [])))
--- modules/HubbleTg.py
+++ modules/HubbleTg.py
@@ -1,5 +1,5 @@
-import json
-import requests
+import aiohttp
+import random
 
 class HubbleTg:
     """Взаимодействие с API телеграм бота"""
@@ -9,23 +9,32 @@ class HubbleTg:
         
         self.token = token
         self.update_id = 0
+        self.session_id = self.get_session_id()
+    
+    def get_session_id(self) -> str:
+        """Генерирует session id"""
         
-    def get_messages(self) -> dict:
+        symbols = ['A', 'E', 'I', 'O', 'U', 'Y', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        session_id = ''
+        for i in range(0, 10):
+            session_id += symbols[random.randint(0, len(symbols) - 1)]
+        return session_id
+        
+    async def get_messages(self) -> dict:
         """Получает непрочитанные сообщения"""
         
         answer = {
             'ok': False,
             'messages': [],
-            'error': None
+            'description': None
         }
         
         try:
-            response = json.loads(requests.get(
-                f'https://api.telegram.org/bot{self.token}/getUpdates',
-                headers={"Content-Type": "application/json"},
-                data=json.dumps({'offset': self.update_id})
-            ).text)
-            
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'https://api.telegram.org/bot{self.token}/getUpdates',
+                                       params={'offset': self.update_id}) as resp:
+                    response = await resp.json()
+
             if 'ok' in response:
                 answer['ok'] = response['ok']
                 
@@ -38,6 +47,39 @@ class HubbleTg:
                         answer['messages'].append(result['message'])
                         
         except Exception as error:
-            answer['error'] = str(error)
-            
+            answer['description'] = str(error)
+        return answer
+    
+    async def send_message(self, text: str, chat_id: int, buttons: list = []) -> dict:
+        """Отправляет сообщение"""
+        answer = {
+            'status_code': None,
+            'description': None
+        }
+        try:
+            if text in [None, '']:
+                answer['status_code'] = 204
+                answer['description'] = 'Пустое сообщение'
+                
+            elif chat_id in [None, 0]:
+                answer['status_code'] = 204
+                answer['description'] = 'Пустой chat_id'
+                
+            else:
+                payload = {
+                    'chat_id': chat_id,
+                    'text': text[:4095], 
+                    'reply_markup': {
+                        'remove_keyboard': True,
+                        'keyboard': [[{'text': text}] for text in buttons] if len(buttons) > 0 else [],
+                        'resize_keyboard': True,
+                        'one_time_keyboard': True
+                    }
+                }
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(f'https://api.telegram.org/bot{self.token}/sendMessage', json=payload) as resp:
+                        answer['status_code'] = resp.status
+        except Exception as error:
+            answer['status_code'] = -1
+            answer['description'] = str(error)
         return answer
